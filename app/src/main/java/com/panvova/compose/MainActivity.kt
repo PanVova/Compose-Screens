31df008685c10c30ed56dbdeb9ad5417023e0181
samurai <panvovandrik@gmail.com>
2022-04-27 22:41:49 +0300

    add Scaffold with top bar

--- app/src/main/java/com/panvova/compose/MainActivity.kt
+++ app/src/main/java/com/panvova/compose/MainActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.panvova.compose.R.string
 import com.panvova.compose.ui.theme.ComposeTheme
 import com.panvova.compose.weather.WeatherScreen
 
@@ -19,9 +27,28 @@ class MainActivity : ComponentActivity() {
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background
         ) {
-          WeatherScreen()
+          ScaffoldWithTopBar()
         }
       }
     }
   }
+}
+
+@Composable
+fun ScaffoldWithTopBar() {
+  Scaffold(
+    topBar = { TopAppBar() },
+    content = {
+      WeatherScreen()
+    })
+}
+
+@Composable
+fun TopAppBar() {
+  TopAppBar(
+    title = { Text(stringResource(string.weather_list)) },
+    actions = {
+      Icon(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null)
+    }
+  )
 }
