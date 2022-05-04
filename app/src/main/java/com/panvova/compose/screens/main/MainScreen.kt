d8b9d5301487c939352377bd343f57120c649dae
samurai <panvovandrik@gmail.com>
2022-05-04 13:38:25 +0300

    add bottomNavigation

--- app/src/main/java/com/panvova/compose/screens/main/MainScreen.kt
+++ app/src/main/java/com/panvova/compose/screens/main/MainScreen.kt
@@ -1,11 +1,19 @@
 package com.panvova.compose.screens.main
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
@@ -15,10 +23,13 @@ import com.panvova.compose.navigation.Screen.ComplexScreen1
 import com.panvova.compose.navigation.Screen.DetailsScreen
 import com.panvova.compose.screens.weather.WeatherScreen
 
+var selectedIndex = mutableStateOf(0)
+
 @Composable
 fun MainScreen(navController: NavController) {
   Scaffold(
     topBar = { TopAppBar(navController) },
+    bottomBar = { BottomAppBar() },
     content = {
       WeatherScreen()
     })
@@ -30,22 +41,47 @@ fun TopAppBar(navController: NavController) {
     title = { Text(stringResource(string.weather_list)) },
     actions = {
       IconButton(onClick = { navController.navigate(DetailsScreen.route) }) {
-        Column() {
-          Icon(
-            painter = painterResource(drawable.ic_launcher_foreground),
-            contentDescription = null
-          )
-        }
+        Icon(
+          painter = painterResource(drawable.ic_launcher_foreground),
+          contentDescription = null
+        )
       }
 
       IconButton(onClick = { navController.navigate(ComplexScreen1.route) }) {
-        Column() {
-          Icon(
-            painter = painterResource(drawable.ic_launcher_background),
-            contentDescription = null
-          )
-        }
+        Icon(
+          painter = painterResource(drawable.ic_launcher_background),
+          contentDescription = null
+        )
       }
     }
   )
+}
+
+@Composable
+fun BottomAppBar() {
+  BottomNavigation() {
+    for (i in 0..3) {
+
+      val (imageVector, name) = when (i) {
+        0 -> Pair(Icons.Filled.Favorite, "Favorite")
+        1 -> Pair(Icons.Filled.Build, "Build")
+        2 -> Pair(Icons.Filled.Call, "Call")
+        else -> Pair(Icons.Filled.Email, "Email")
+      }
+
+      BottomNavigationItem(
+        icon = {
+
+          Icon(imageVector = imageVector, contentDescription = null)
+        },
+        label = {
+          Text(text = name)
+        },
+        selected = selectedIndex.value == i,
+        onClick = { selectedIndex.value = i },
+        selectedContentColor = Color.Green,
+        unselectedContentColor = Color.White
+      )
+    }
+  }
 }
